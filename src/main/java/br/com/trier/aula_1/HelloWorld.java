bfb7899aa3c3ca9bb97a5608bf3e311deab246be
Sandro Cunha <sandromedeirosdacunha@gmail.com>
2023-05-22 15:51:11 -0300

    Segundo commit - Teste

--- src/main/java/br/com/trier/aula_1/HelloWorld.java
+++ src/main/java/br/com/trier/aula_1/HelloWorld.java
@@ -3,6 +3,7 @@ package br.com.trier.aula_1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World!!!!!!!!!!!!");
+		System.out.println("Testando");
 	}
 }
